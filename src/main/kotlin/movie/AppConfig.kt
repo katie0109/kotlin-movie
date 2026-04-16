28571d66f329d9da061aa097aa7064e67071ec0a
katie0109 <0109katie@naver.com>
2026-04-16 21:24:13 +0900

    feat: AppConfig.kt — Connection, Repository, PriceCalculator를 Spring Bean으로 등록

---
+++ src/main/kotlin/movie/AppConfig.kt
@@ -0,0 +1,59 @@
+package movie
+
+import movie.domain.discount.DiscountPolicies
+import movie.domain.discount.DiscountPolicy
+import movie.domain.discount.MovieDayDiscount
+import movie.domain.discount.TimeDiscount
+import movie.domain.payment.PriceCalculator
+import movie.infrastructure.db.DatabaseConnector
+import movie.infrastructure.db.DatabaseInitializer
+import movie.infrastructure.db.JdbcMovieRepository
+import movie.infrastructure.db.JdbcReservationRepository
+import movie.infrastructure.db.JdbcReservedSeatRepository
+import movie.infrastructure.db.JdbcScreeningRepository
+import org.springframework.context.annotation.Bean
+import java.sql.Connection
+
+class AppConfig{
+
+    @Bean
+    fun connection(): Connection {
+        return DatabaseConnector.connectLocal()
+    }
+
+    @Bean
+    fun databaseInitializer(connection: Connection): DatabaseInitializer {
+        return DatabaseInitializer(connection)
+    }
+
+    @Bean
+    fun movieRepository(connection: Connection, screeningRepository: JdbcScreeningRepository): JdbcMovieRepository {
+        return JdbcMovieRepository(connection, screeningRepository)
+    }
+
+    @Bean
+    fun reservedSeatRepository(connection: Connection): JdbcReservedSeatRepository {
+        return JdbcReservedSeatRepository(connection)
+    }
+
+    @Bean
+    fun screeningRepository(connection: Connection, reservedSeatRepository: JdbcReservedSeatRepository): JdbcScreeningRepository {
+        return JdbcScreeningRepository(connection, reservedSeatRepository)
+    }
+
+    @Bean
+    fun reservationRepository(connection: Connection, reservedSeatRepository: JdbcReservedSeatRepository): JdbcReservationRepository {
+        return JdbcReservationRepository(connection, reservedSeatRepository)
+    }
+
+    @Bean
+    fun priceCalculator(): PriceCalculator {
+        return PriceCalculator(
+            discountPolicies = DiscountPolicies(
+                listOf(MovieDayDiscount()),
+                listOf(TimeDiscount())
+            )
+        )
+    }
+
+}
